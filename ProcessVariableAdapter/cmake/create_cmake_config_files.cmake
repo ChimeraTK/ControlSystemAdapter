210f2983dddd55cff9d91c324351a078c448618e
killenb <killenb@5f601a5c-88b9-47f3-b851-818f6741d00d>
2015-03-27 16:11:26 +0000

    Implemented proper installation, incl. CMake find scripts
    git-svn-id: https://svnsrv.desy.de/desy/mtca4u/ControlSystemTools/trunk@906 5f601a5c-88b9-47f3-b851-818f6741d00d

---
+++ ProcessVariableAdapter/cmake/create_cmake_config_files.cmake
@@ -0,0 +1,29 @@
+#check that all config variables which have to be replaced are set
+foreach( CONFIG_VARIABLE CONFIG_INCLUDE_DIRS 
+                         REFERENCE_HEADER
+			 CONFIG_LIBRARIES
+			 CONFIG_LIBRARY_DIRS )
+  if(NOT ${PROJECT_NAME}_${CONFIG_VARIABLE})
+    message(FATAL_ERROR "${PROJECT_NAME}_${CONFIG_VARIABLE} not set in CMakeListst.txt")
+  endif(NOT ${PROJECT_NAME}_${CONFIG_VARIABLE})
+endforeach(CONFIG_VARIABLE)
+
+#We have nested @-statements, so we have to parse twice
+configure_file(cmake/PROJECT_NAMEConfig.cmake.in.in
+  "${PROJECT_BINARY_DIR}/cmake/${PROJECT_NAME}Config.cmake.in" @ONLY)
+configure_file(${PROJECT_BINARY_DIR}/cmake/${PROJECT_NAME}Config.cmake.in
+  "${PROJECT_BINARY_DIR}/${PROJECT_NAME}Config.cmake" @ONLY)
+
+configure_file(cmake/PROJECT_NAMEConfigVersion.cmake.in.in
+  "${PROJECT_BINARY_DIR}/cmake/${PROJECT_NAME}ConfigVersion.cmake.in" @ONLY)
+configure_file(${PROJECT_BINARY_DIR}/cmake/${PROJECT_NAME}ConfigVersion.cmake.in
+  "${PROJECT_BINARY_DIR}/${PROJECT_NAME}ConfigVersion.cmake" @ONLY)
+
+configure_file(cmake/FindPROJECT_NAME.cmake.in
+  "${PROJECT_BINARY_DIR}/Find${PROJECT_NAME}.cmake" @ONLY)
+
+# Install the ..Config.cmake and ..ConfigVersion.cmake
+install(FILES "${PROJECT_BINARY_DIR}/${PROJECT_NAME}ConfigVersion.cmake"
+  "${PROJECT_BINARY_DIR}/${PROJECT_NAME}Config.cmake" 
+  "${PROJECT_BINARY_DIR}/Find${PROJECT_NAME}.cmake"
+  DESTINATION . COMPONENT dev)
