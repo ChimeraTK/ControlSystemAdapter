c683732303e18f98c6b3f77f7971aefc7290870b
killenb <killenb@5f601a5c-88b9-47f3-b851-818f6741d00d>
2016-04-22 17:16:00 +0000

    added the COntrol System Adapter DEmo (cosade)
    git-svn-id: https://svnsrv.desy.de/desy/mtca4u/ControlSystemTools/trunk@2781 5f601a5c-88b9-47f3-b851-818f6741d00d

---
+++ ProcessVariableAdapter/examples/cosade/IndependentControlCore.h
@@ -0,0 +1,88 @@
+#ifndef _INDEPENDENT_CONTOL_CORE_H_
+#define _INDEPENDENT_CONTOL_CORE_H_
+
+#include <boost/scoped_ptr.hpp>
+
+//FIXME: in a real example an installed version of the adapter has to be used,
+//which has the headers installed under the namespace folder
+//#include <ControlSystemAdapter/DevicePVManager.h>
+//#include <ControlSystemAdapter/ProcessScalar.h>
+//#include <ControlSystemAdapter/DeviceSynchronizationUtility.h>
+//#include <ControlSystemAdapter/SynchronizationDirection.h>
+#include <DevicePVManager.h>
+#include <ProcessScalar.h>
+#include <DeviceSynchronizationUtility.h>
+#include <SynchronizationDirection.h>
+
+/** Some dummy "hardware". You can read/write a voltage (int). */
+class Hardware{
+  int _voltage;
+ public:
+  void setVoltage(int v){_voltage=v;}
+  int getVoltage() const {return _voltage;}
+ Hardware(): _voltage(42){}
+};
+
+/** This is just a simple example class.
+ *
+ *  All functions are definded inline for the sake of the example. 
+ *  It is strongly recommended to use proper header/object separation for
+ *  real code!
+ */
+class IndependentControlCore{
+ private:
+  mtca4u::DevicePVManager::SharedPtr _processVariableManager;
+
+  /** The target voltage to be transmitted to the hardware */
+  mtca4u::ProcessScalar<int>::SharedPtr _targetVoltage;
+
+  /** The monitor voltage which is read back from the hardware */
+  mtca4u::ProcessScalar<int>::SharedPtr _monitorVoltage;
+  
+  Hardware _hardware; ///< Some hardware
+ 
+  boost::scoped_ptr< boost::thread > _deviceThread;
+
+  void mainLoop();
+
+ public:
+  /** The constructor gets an instance of the variable factory to use. 
+   *  The variables in the factory should already be initialised because the hardware is initialised here.
+   */
+  IndependentControlCore(boost::shared_ptr<mtca4u::DevicePVManager> const & processVariableManager)
+    //initialise all process variables, using the factory
+    : _processVariableManager( processVariableManager ),
+    _targetVoltage( processVariableManager->createProcessScalar<int>(mtca4u::controlSystemToDevice,"TARGET_VOLTAGE") ),
+    _monitorVoltage( processVariableManager->createProcessScalar<int>(mtca4u::deviceToControlSystem,"MONITOR_VOLTAGE") ){
+
+    // initialise the hardware here
+    *_targetVoltage = 0;
+    *_monitorVoltage = 0;
+    _hardware.setVoltage(*_targetVoltage);
+
+    // start the device thread, which is executing the main loop
+    _deviceThread.reset( new boost::thread( boost::bind( &IndependentControlCore::mainLoop, this ) ) );
+  }
+  
+  ~IndependentControlCore(){
+    // stop the device thread before any other destructors are called
+    _deviceThread->interrupt();
+    _deviceThread->join();
+ }
+
+};
+
+inline void IndependentControlCore::mainLoop(){
+  mtca4u::DeviceSynchronizationUtility syncUtil(_processVariableManager);
+ 
+  while (!boost::this_thread::interruption_requested()) {
+    syncUtil.receiveAll();
+    *_monitorVoltage = _hardware.getVoltage();
+
+    _hardware.setVoltage( *_targetVoltage );
+    syncUtil.sendAll();
+    boost::this_thread::sleep_for( boost::chrono::milliseconds(100) );
+  }
+}
+
+#endif // _INDEPENDENT_CONTOL_CORE_H_
---
+++ ProcessVariableAdapter/examples/cosade/testIndependentControlCore.cpp
@@ -0,0 +1,76 @@
+// test the IndependentControlCore without DOOCS
+
+#define BOOST_TEST_MODULE IndependentControlCoreTest
+// Only after defining the name include the unit test header.
+#include <boost/test/included/unit_test.hpp>
+
+//FIXME: in a real example an installed version of the adapter has to be used,
+//which has the headers installed under the namespace folder
+//#include <ControlSystemAdapter/ControlSystemSynchronizationUtility.h>
+#include <ControlSystemSynchronizationUtility.h>
+
+#include "IndependentControlCore.h"
+
+using namespace boost::unit_test_framework;
+using namespace mtca4u;
+
+BOOST_AUTO_TEST_SUITE( IndependentControlCoreTestSuite )
+
+BOOST_AUTO_TEST_CASE( independentControlCoreTest ){
+  std::pair<boost::shared_ptr<ControlSystemPVManager>,
+	    boost::shared_ptr<DevicePVManager> > pvManagers = createPVManager();
+  boost::shared_ptr<ControlSystemPVManager> csManager = pvManagers.first;
+  boost::shared_ptr<DevicePVManager> devManager = pvManagers.second;
+
+  IndependentControlCore controlCore(devManager);
+
+  ControlSystemSynchronizationUtility syncUtil(csManager);
+
+  ProcessScalar<int>::SharedPtr targetVoltage 
+    = csManager->getProcessScalar<int>("TARGET_VOLTAGE");
+  ProcessScalar<int>::SharedPtr monitorVoltage 
+    = csManager->getProcessScalar<int>("MONITOR_VOLTAGE");
+
+  // start with -1 for both voltages
+  *targetVoltage = -1;
+  *monitorVoltage = -1;
+
+  // the other side has a timeout of 100 ms, so we wait 1 second to be safe,
+  // but we look every 10 ms (not too frequent, but only with 10 % of additional latency)
+  //syncUtil.waitForNotifications(1000000, 10000);
+  //Note: as waitForNotification does not work as expected, the functionality is
+  // done manually for just the one variable we are receiving.
+  for (size_t i=0; i<  100; ++i){
+    boost::this_thread::sleep_for( boost::chrono::milliseconds(10) );
+    if (monitorVoltage->receive()){
+      break;
+    }
+  }
+
+  // the target voltage must not have changed
+  BOOST_CHECK(  *targetVoltage == -1 );
+  // the monitor voltage has to be 0 now, like in the hardware
+  BOOST_CHECK(  *monitorVoltage == 0 );
+
+  *targetVoltage = 42;
+  targetVoltage->send();
+
+  // Wait until we read something back and check that the expected value is there.
+  // This is not nevessarily the case on the first read, so we put a limit to 10.
+  // Note that the number of actual receive attempty is higher, because we 
+  // read every 10 ms while the other side is sending every 100 ms.
+  for (size_t receiveCounter=0; receiveCounter<  10; /*empty*/){
+    boost::this_thread::sleep_for( boost::chrono::milliseconds(10) );
+    if (monitorVoltage->receive()){
+      ++receiveCounter;
+      if (*monitorVoltage == 42){
+	break;
+      }
+    }
+  }
+
+  BOOST_CHECK(*monitorVoltage == 42);
+  
+}
+
+BOOST_AUTO_TEST_SUITE_END()
