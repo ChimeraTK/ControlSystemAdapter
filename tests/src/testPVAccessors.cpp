ba7013a2485827818447d92942687a26ef17a57a
Martin <martin.killenberg@desy.de>
2016-06-11 14:09:32 +0200

    testing throwing if the wrong type is replaced

--- tests/src/testPVAccessors.cpp
+++ tests/src/testPVAccessors.cpp
@@ -3,6 +3,7 @@
 #include <boost/test/included/unit_test.hpp>
 
 #include "ProcessScalarAccessor.h"
+#include "ProcessArray.h"
 
 using namespace mtca4u;
 
@@ -32,6 +33,17 @@ BOOST_AUTO_TEST_CASE( testScalarCreation ){
   
   scalarIntAccessor.set(44);
   BOOST_CHECK( secondIntAccessor.get() == 44 );
+
+  auto intArray = createSimpleProcessArray<int>(10, "intArray");
+  BOOST_CHECK_THROW( secondIntAccessor.replace(intArray), std::runtime_error) ;
+
+  auto scalarFloat = createSimpleProcessScalar<float>("scalarFloat");
+  BOOST_CHECK_THROW( secondIntAccessor.replace(scalarFloat), std::runtime_error) ;
+}
+
+BOOST_AUTO_TEST_CASE( testPVAccessor ){
+  // testing the base class. 
+  
 }
 
 BOOST_AUTO_TEST_SUITE_END()
