025b7ccb54d13eb79038d3c75d29755b4f82d161
Martin Killenberg <martin.killenberg@desy.de>
2016-06-13 15:42:48 +0200

    Revert "Introduced intermediate class NonModifiableProcessArrayAccessor. Needed to keep the readback promise for non-swappable ProcessArrays."
    This reverts commit 3f0861170277a67910e087d0a8523a6eb4a38b6f.
    The new implementation will not have the 'non-swappable' promise any more, so we don't need it.

--- include/ProcessArrayAccessor.h
+++ include/ProcessArrayAccessor.h
@@ -6,37 +6,17 @@
 
 namespace mtca4u{
   template<class T>
-    class NonModifiableProcessArrayAccessor: public ProcessVariableAccessor{
+    class ProcessArrayAccessor: public ProcessVariableAccessor{
 
   public:
-  NonModifiableProcessArrayAccessor(typename ProcessArray<T>::SharedPtr const & processArray =
+  ProcessArrayAccessor(typename ProcessArray<T>::SharedPtr const & processArray =
 			typename ProcessArray<T>::SharedPtr())
     : ProcessVariableAccessor(processArray){
       //      _processArray( boost::static_pointer_cast< ProcessArray<T> >( _impl ) ){      
     }
 
-    std::vector<T> const & get() const{
-      return boost::static_pointer_cast< ProcessArray<T> >(_impl)->get();
-    }
-
-    std::vector<T> const & getConst() const{
-      return boost::static_pointer_cast< ProcessArray<T> >(_impl)->getConst();
-    }
-    
-  };
-
-  template<class T>
-    class ProcessArrayAccessor: public NonModifiableProcessArrayAccessor<T>{
-
-  public:
-  ProcessArrayAccessor(typename ProcessArray<T>::SharedPtr const & processArray =
-			typename ProcessArray<T>::SharedPtr())
-    : NonModifiableProcessArrayAccessor<T>(processArray){
-      //      _processArray( boost::static_pointer_cast< ProcessArray<T> >( _impl ) ){      
-    }
-
     std::vector<T> & get(){
-      return boost::static_pointer_cast< ProcessArray<T> >(ProcessVariableAccessor::_impl)->get();
+      return boost::static_pointer_cast< ProcessArray<T> >(_impl)->get();
     }
     
   };
