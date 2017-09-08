90761a0be012fc947fb46348b9c5614696f68650
Martin Killenberg <martin.killenberg@desy.de>
2017-09-08 10:27:47 +0200

    refactored some tests for simpler code and better stability

--- tests/src/testTypeChangingDecorator.cpp
+++ tests/src/testTypeChangingDecorator.cpp
@@ -8,14 +8,18 @@ using namespace ChimeraTK;
 
 #include <mtca4u/Device.h>
 
+bool test_close(double a, double b, double tolerance = 0.0001){
+  return (std::fabs(a - b) < tolerance );
+}
+
 BOOST_AUTO_TEST_SUITE( TypeChangingDecoratorTestSuite )
 
 template<class T, class IMPL_T>
-void testDecorator(IMPL_T startReadValue, T expectedReadValue, T startWriteValue, IMPL_T exprectedWriteValue){
+void testDecorator(double startReadValue, T expectedReadValue, T startWriteValue, double expectedWriteValue){
   mtca4u::Device d;
   d.open("sdm://./dummy=decoratorTest.map");
   auto scalar = d.getScalarRegisterAccessor<IMPL_T>("/SOME/SCALAR");
-  auto anotherScalarAccessor = d.getScalarRegisterAccessor<IMPL_T>("/SOME/SCALAR");
+  auto anotherScalarAccessor = d.getScalarRegisterAccessor<double>("/SOME/SCALAR");
   
   auto ndAccessor = boost::dynamic_pointer_cast<NDRegisterAccessor< IMPL_T > >(scalar.getHighLevelImplElement());
   TypeChangingDecorator<T, IMPL_T> decoratedScalar(ndAccessor);
@@ -25,45 +29,17 @@ void testDecorator(IMPL_T startReadValue, T expectedReadValue, T startWriteValue
   
   anotherScalarAccessor = startReadValue;
   anotherScalarAccessor.write();
-  //  BOOST_CHECK(scalar == 0);
-  //  BOOST_CHECK(decoratedScalar.accessData(0) == 0);
+  // check that the values are different at start so we know the test is sensitive
+  BOOST_CHECK( !test_close(decoratedScalar.accessData(0), expectedReadValue) );
   decoratedScalar.read();
-  BOOST_CHECK( expectedReadValue == decoratedScalar.accessData(0) );
+  // internal precision of the register is 16 fractional bits fixed point
+  BOOST_CHECK( test_close(decoratedScalar.accessData(0), expectedReadValue) );
 
   decoratedScalar.accessData(0) = startWriteValue;
   decoratedScalar.write();
   anotherScalarAccessor.read();
-  BOOST_CHECK( exprectedWriteValue == anotherScalarAccessor );
+  BOOST_CHECK( test_close(anotherScalarAccessor, expectedWriteValue) );
 }
-
-template<>
-void testDecorator(std::string startReadValue, int expectedReadValue, int startWriteValue, std::string exprectedWriteValue){
-  mtca4u::Device d;
-  d.open("sdm://./dummy=decoratorTest.map");
-  auto scalar = d.getScalarRegisterAccessor<std::string>("/SOME/SCALAR");
-  auto anotherScalarAccessor = d.getScalarRegisterAccessor<std::string>("/SOME/SCALAR");
-  
-  auto ndAccessor = boost::dynamic_pointer_cast<NDRegisterAccessor< std::string > >(scalar.getHighLevelImplElement());
-  TypeChangingDecorator<int, std::string> decoratedScalar(ndAccessor);
-
-  BOOST_REQUIRE( decoratedScalar.getNumberOfChannels()==1);
-  BOOST_REQUIRE( decoratedScalar.getNumberOfSamples()==1);
-  
-  anotherScalarAccessor = startReadValue;
-  anotherScalarAccessor.write();
-  //  BOOST_CHECK(scalar == 0);
-  //  BOOST_CHECK(decoratedScalar.accessData(0) == 0);
-  decoratedScalar.read();
-  BOOST_CHECK( expectedReadValue == decoratedScalar.accessData(0) );
-
-  decoratedScalar.accessData(0) = startWriteValue;
-  decoratedScalar.write();
-  anotherScalarAccessor.read();
-  std::string readResponse = anotherScalarAccessor;
-  BOOST_CHECK( readResponse == exprectedWriteValue );
-}
-
-
 
 BOOST_AUTO_TEST_CASE( testAllDecorators ){
   testDecorator<int, int8_t>(12,12, 22, 22);
@@ -75,7 +51,7 @@ BOOST_AUTO_TEST_CASE( testAllDecorators ){
   // only use values which habe an exact representation in float/fixed point
   testDecorator<int, float>(18.5,19, 28, 28);
   testDecorator<int, double>(19.5,20, 29, 29);
-  testDecorator<int, std::string>(std::string("101"), 101, 102, std::string("102.000000"));
+  testDecorator<int, std::string>(101, 101, 102, 102);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
