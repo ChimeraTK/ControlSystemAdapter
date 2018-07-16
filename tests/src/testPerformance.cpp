81f26ac8d83e977d7a26ea8e1ef590e2e5e1c92f
Martin Hierholzer <martin.hierholzer@desy.de>
2018-07-16 15:39:26 +0200

    performance test is now using arrays instead of scalars

--- tests/src/testPerformance.cpp
+++ tests/src/testPerformance.cpp
@@ -7,22 +7,27 @@ using namespace ChimeraTK;
 
 int main() {
 
-    constexpr size_t nVars = 1000;
-    constexpr size_t nSendsPerVar = 1000;
+    constexpr size_t dataSize = 16384;
+    constexpr size_t nVars = 200;
+    constexpr size_t nSendsPerVar = 2000;
 
     // create process variable pairs
     std::vector< std::pair< boost::shared_ptr<ProcessArray<size_t>>, boost::shared_ptr<ProcessArray<size_t>> > > pvars;
     for(size_t k=0; k<nVars; ++k) {
       std::string name = "var"+std::to_string(k);
-      pvars.push_back(createSynchronizedProcessArray<size_t>(1, name));
+      pvars.push_back(createSynchronizedProcessArray<size_t>(dataSize, name));
     }
 
     // create sender thread
-    boost::thread sender( [pvars] {
+    size_t sum_sender = 0;
+    boost::thread sender( [pvars, &sum_sender] {
       for(size_t i=0; i<nSendsPerVar; ++i) {
         size_t k=0;
         for(auto &pv : pvars) {
-          pv.first->accessData(0) = k*nSendsPerVar + i;
+          for(size_t j=0; j<dataSize; ++j) {
+            pv.first->accessData(j) = k*nSendsPerVar + i + j;
+            sum_sender += k*nSendsPerVar + i + j;
+          }
           while(true) {
             bool dataLost = pv.first->write();
             if(!dataLost) break;
@@ -33,18 +38,19 @@ int main() {
 
     auto start = std::chrono::steady_clock::now();
 
-    size_t error_count = 0;
+    size_t sum = 0;
     for(size_t i=0; i<nSendsPerVar; ++i) {
-      size_t k=0;
       for(auto &pv : pvars) {
         pv.second->read();
-        if( pv.second->accessData(0) < k*nSendsPerVar + i || pv.second->accessData(0) > k*nSendsPerVar + i + 1 ) ++error_count;
+        for(size_t j=0; j<dataSize; ++j) sum += pv.second->accessData(j);
       }
     }
-
-    if(error_count > 0) {
-      std::cout << "*** Test FAILED. " << error_count << " errors found." << std::endl;
-      return 1;
+    std::cout << "SUM = " << sum << std::endl;
+    sender.join();
+    bool failed = false;
+    if(sum_sender != sum) {
+      std::cout << "ERROR sender sum = " << sum_sender << std::endl;
+      failed = true;
     }
 
     auto end = std::chrono::steady_clock::now();
@@ -56,6 +62,6 @@ int main() {
     std::cout << "Average time per transfer: " << diff.count()/(double)nTransfers * 1e6 << " us\n";
 
 
-    return 0;
+    return failed;
 
 }
