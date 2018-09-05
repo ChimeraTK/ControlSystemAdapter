6cfb73eec1c1ab7fc7da72b101d5fcba3d3a4442
Martin Hierholzer <martin.hierholzer@desy.de>
2018-09-05 14:26:00 +0200

    corrected wrong header file added in list commit

--- tests/src/testUnidirectionalProcessArray.cpp
+++ tests/src/testUnidirectionalProcessArray.cpp
@@ -12,7 +12,7 @@ using namespace boost::unit_test_framework;
 
 #include "CountingProcessVariableListener.h"
 
-#include <ChimeraTK/SupportedUserTypes.h>
+#include <boost/mpl/list.hpp>
 
 typedef boost::mpl::list<int8_t,uint8_t,
                          int16_t,uint16_t,
